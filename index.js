26f064c06333a78a2e25e783d8336d90c15854df
Aleksander Shustov <krisale@yandex.ru>
2024-10-23 19:36:05 +0300

    feat: create ws

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from './src/http_server/index.js';
+import './src/ws_server/ws_server.js';
 
 const HTTP_PORT = 8181;
 
